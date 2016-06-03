7a867a8d6fe7d0fe83b759f65b777c908ddb31cf
W. X. Chan <w.x.chan1986@gmail.com>
2016-06-03 12:32:18 +0800

    Update example1.py

--- example1.py
+++ example1.py
@@ -12,7 +12,7 @@ import numpy as np
 import autoD as ad
 
 x=ad.Scalar('x')
-a=ad.Multiply([2.,ad.Cos(ad.Power(x,2.))])
+a=2.*ad.Cos(x**2.)
 b=ad.Differentiate(ad.Sin(ad.Ln(x)),1)
-fx=ad.Addition([a,ad.Multiply([-1,b])])
+fx=a-b
 print(fx.cal({'x':0.2},{'x':1}))
