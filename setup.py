0d814682d87ba12d97f31755624f7becf86d991f
W. X. Chan <w.x.chan1986@gmail.com>
2019-11-20 16:55:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'autoD',         # How you named your package folder (MyLib)
   packages = ['autoD'],   # Chose the same as "name"
-  version = '3.9.1',      # Start with a small number and increase it with every change you make
+  version = '3.9.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Forward automatic differentiation',   # Give a short description about your library
   author = 'Wei Xuan Chan',                   # Type in your name
   author_email = 'w.x.chan1986@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/WeiXuanChan/autoD',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/WeiXuanChan/autoD/archive/v3.9.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/WeiXuanChan/autoD/archive/v3.9.2.tar.gz',    # I explain this later on
   keywords = ['automatic', 'differentiation'],   # Keywords that define your package best
   install_requires=['numpy'],
           
